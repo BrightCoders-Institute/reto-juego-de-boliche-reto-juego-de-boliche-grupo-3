04e70712630ce8b471f3f1f78022846337c4aad6
ruruisu <zaddozgl@gmail.com>
2023-06-06 16:44:23 -0600

    initial code

---
+++ main.js
@@ -0,0 +1,26 @@
+class BowlingGame{
+    constructor() {
+        this.frames = Array(10)
+    }
+
+    playFrames(){
+        for (let i = 0; i < this.frames.length; i++) {
+            
+        }
+    }
+    calculatePoints() {
+
+    }
+
+    isStrike() {
+
+    }
+
+    isPar() {
+
+    }
+
+    isLastFrame(){
+
+    }
+}
