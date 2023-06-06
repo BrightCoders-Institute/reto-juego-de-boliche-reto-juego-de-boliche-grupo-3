405448f9a450ff9612b7bbe8067cdce3e11d0a6f
ruruisu <zaddozgl@gmail.com>
2023-06-06 16:44:44 -0600

    initial code
    Co-authored-by: madera-design <joseantoniomaderag@gmail.com>
    Co-authored-by: Raibrr <ramon.ibrr@gmail.com>

--- main.js
+++ main.js
@@ -2,9 +2,16 @@ class BowlingGame{
     constructor() {
         this.frames = Array(10)
     }
-
-    playFrames(){
+    getRandomInt(max) {
+        return Math.floor(Math.random() * max);
+      }
+    fillFrames() {
+    }
+    playGame(){
+        let score = 0
         for (let i = 0; i < this.frames.length; i++) {
+
+            this.getRandomInt(10)
             
         }
     }
